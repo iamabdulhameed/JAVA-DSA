1fdbf9441bb20857658cb8bd34a913bc088bc51b
abdul-hameed-layr <abdul.hameed@layrins.com>
2023-12-05 17:18:20 +0530

    Initial Setup

---
+++ src/main/java/com/random/Random1.java
@@ -0,0 +1,11 @@
+package com.random;
+
+import org.testng.annotations.Test;
+
+public class Random1 {
+
+    @Test
+    public void firstTest() {
+        System.out.println("Hello, World!");
+    }
+}
